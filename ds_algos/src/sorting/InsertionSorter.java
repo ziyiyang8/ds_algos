81ef91b9464a3ab7aa7d09c70d0f436d395f522c
Ziyi <Ziyi@DESKTOP-NVVBMNN>
2016-05-10 20:56:05 -0700

    more data structures

--- ds_algos/src/sorting/InsertionSorter.java
+++ ds_algos/src/sorting/InsertionSorter.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 public class InsertionSorter {
 	
 	public static void main(String[] args) {
-		int[] data = {7,3,6,8,2};
+		Integer[] data = {7,3,6,8,2};
 		new InsertionSorter().sort(data);
 		System.out.println(Arrays.toString(data));
 	}
 	
-	public void sort(int[] data) {
+	public void sort(Integer[] data) {
 		for (int i =0; i < data.length; i++) {
 			int current = data[i];
 			int j = i-1;
